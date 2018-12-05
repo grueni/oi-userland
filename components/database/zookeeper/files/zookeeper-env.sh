5953999fc6a86a35e0128246a0b294b5c7b1c15d
Andreas Grueninger <andreas.grueninger@nomail.com>
2018-12-05 09:24:09 +0100

    zookeeper: add version 3.4.13

---
+++ components/database/zookeeper/files/zookeeper-env.sh
@@ -0,0 +1,25 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+export JAVA_HOME=/usr/jdk/openjdk1.8.0
+export ZOO_LOG4J_PROP=INFO,ROLLINGFILE
+export ZOO_LOG_DIR=/var/zookeeper/log
+export ZOO_TMP_DIR=/var/zookeeper/tmp
+export ZOOCFGDIR=/etc/zookeeper
+export JMXLOCALONLY=true
+export JMXDISABLE=false
+export JMXPORT=4048
+export JMXAUTH=false
+export JMXSSL=false
+
